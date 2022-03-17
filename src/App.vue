acd46dbfacec98078302e4e06e30b93c9724d14b
fabiochidda <fabio.chidda99@gmail.com>
2022-03-17 01:20:06 +0100

    Ricavati i titoli dei film tramite l'API e stampati

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,38 @@
 <template>
   <div id="app">
 
+    <div class="search-bar">
+      <input type="text" v-model="search">
+    </div>
+
+    <div class="movie-container">
+      <div class="movie" v-for="el in movies" :key="el.id">
+        {{el.title}}
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   name: 'App',
-  components: {
+  data() {
+    return {
+      search: 'men in black',
+      movies: []
+    }
+  },
+  created() {
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=df1607d64d3add4c3ecca5022fee5cce&query=${ this.search }`)
+    .then( res => {
+      this.movies = res.data.results
+    })
+    .catch( error => {
+      console.log(error.response)
+    })
   }
 }
 </script>
